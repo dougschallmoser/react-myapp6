822a0884ce275b7a9c13642e6760ef9eb82f7ccb
Doug Schallmoser <dougschallmoser@mgail.com>
2020-11-02 16:32:48 -0800

    add api fetch and build out convert component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,14 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('')
 
   useEffect(() => {
-    console.log('new language or text')
+    const translate = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      })
+
+      setTranslated(data.data.translations[0].translatedText)
+    }
+
+    const timeoutId = setTimeout(() => {
+      if (text) {
+        translate()
+      }
+    }, 500)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+
   }, [language, text])
 
   return (
     <div>
-
+      <h1 className="ui header">{translated}</h1>
     </div>
   )
 }
